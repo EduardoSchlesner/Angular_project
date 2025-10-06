6c2a2188c1d1029c75d65266684f3ce30d18655c
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-10-06 15:26:16 -0300

    Definindo componente novos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AluraPic';
-  description = 'Leão';
-  url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWkLpWu33R94SgmeLfS8EtXhLRKiB6BZAZ5A&s';
+
 }
