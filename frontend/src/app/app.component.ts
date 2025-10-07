f42dfefbbe34f2eee92038827b023153d174f545
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-10-07 15:48:52 -0300

    Adicionando um arquivo para a lista de fotos.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,8 @@
-import {Component, Injectable, OnInit} from '@angular/core';
-import {PhotoService} from './photos/photo/photo.service';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  photos: any[] = [];
-  constructor(private photoService: PhotoService) {}
-  ngOnInit() {
-    this.photoService
-      .listFromUser('flavio')
-      .subscribe(photos => this.photos = photos);
-  }
-}
+export class AppComponent {}
