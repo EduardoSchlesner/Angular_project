b939ac6a91385f4045c36133623c3fcd8482a574
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-10-08 15:59:47 -0300

    Adicionando funções ao filtro e uma mensagem caso nenhum imagem seja encontrada.

--- frontend/src/app/photos/photo-list/photo-list.component.ts
+++ frontend/src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {PhotoService} from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import {Photo} from '../photo/photo';
 
 @Component({
@@ -8,20 +10,22 @@ import {Photo} from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter = '';
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+
+  debounce: Subject<string> = new Subject<string>();
+  private debounceSubscription: Subscription;
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
   ngOnInit() {
-    const userName = this.activatedRoute
-      .snapshot
-      .params
-      .userName;
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounceSubscription = this.debounce
+      .pipe(debounceTime(300)) // Espera 300ms após o usuário parar de digitar
+      .subscribe(filterValue => this.filter = filterValue);
+  }
+  ngOnDestroy(): void {
+    this.debounceSubscription.unsubscribe();
   }
 }
