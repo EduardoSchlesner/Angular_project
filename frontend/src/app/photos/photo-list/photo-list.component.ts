92bba70fa19eaa31fa5f31a7b9b6901bfc131d33
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-10-08 17:06:01 -0300

    Botão de load adicionado e carregando mais imagens.

--- frontend/src/app/photos/photo-list/photo-list.component.ts
+++ frontend/src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Subject, Subscription } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import {Photo} from '../photo/photo';
+import {PhotoService} from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -17,9 +18,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   debounce: Subject<string> = new Subject<string>();
   private debounceSubscription: Subscription;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  hasMore = true;
+  currentPage: numver = 1;
+  userName = '';
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private photoService: PhotoService
+  ) {}
 
   ngOnInit() {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
     this.debounceSubscription = this.debounce
       .pipe(debounceTime(300)) // Espera 300ms após o usuário parar de digitar
@@ -27,5 +35,15 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   }
   ngOnDestroy(): void {
     this.debounceSubscription.unsubscribe();
+  }
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      });
   }
 }
