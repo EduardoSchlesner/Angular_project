5f6ebea61e96819bd1dbcebb57afbe2aa7ba31d5
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-10-14 16:20:13 -0300

    Criando uma aba de registro para novos usuários.

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,11 +1,22 @@
 import {NgModule} from '@angular/core';
 import {SignInComponent} from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {VMessageModule} from '../shared/component/vmessage/vmessage.module';
+import {SignupComponent} from './signup/signup.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [ SignInComponent ],
-  imports: [ReactiveFormsModule, CommonModule, VMessageModule]
+  declarations: [
+    SignInComponent,
+    SignupComponent
+  ],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    VMessageModule,
+    RouterModule
+  ]
 })
 export class HomeModule { }
